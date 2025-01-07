ea07cb28ac018f5c4b38a1343258624f0dd3be5a
Matthew Lyle <matthew.lyle@icloud.com>
2025-01-07 11:47:30 +0000

    Setup com.xml and folder structure

---
+++ src/main/java/com/MatthewLyle17/Program.java
@@ -0,0 +1,10 @@
+package com.MatthewLyle17;
+public class Program{
+
+    public static void main(String[] args) {
+        
+        System.out.println("This is a Maven project in the package com.MatthewLyle17");
+
+    }
+
+}
